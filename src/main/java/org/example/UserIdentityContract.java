40d80474a4b5da8fe1408f805091996a4d5bc14e
RAFAELrodr <rafaa.verdao@gmail.com>
2020-09-17 16:47:31 -0300

    create query method

--- src/main/java/org/example/UserIdentityContract.java
+++ src/main/java/org/example/UserIdentityContract.java
@@ -3,31 +3,41 @@
  */
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 @org.hyperledger.fabric.contract.annotation.Contract(name = "UserIdentityContract",
-    info = @Info(title = "UserIdentity contract",
+        info = @Info(title = "UserIdentity contract",
                 description = "My Smart Contract2",
                 version = "0.0.1",
                 license =
-                        @License(name = "Apache-2.0",
-                                url = ""),
-                                contact =  @Contact(email = "Tcc@example.com",
-                                                name = "Tcc",
-                                                url = "http://Tcc.me")))
+                @License(name = "Apache-2.0",
+                        url = ""),
+                contact = @Contact(email = "Tcc@example.com",
+                        name = "Tcc",
+                        url = "http://Tcc.me")))
 @Default
 public class UserIdentityContract implements ContractInterface {
-    public UserIdentityContract() {}
+    public UserIdentityContract() {
+    }
 
     /**
      * O metodo returna true se userSignId passado como parametro existe
      * e false caso nao exista
+     *
      * @param ctx
      * @param userSignId
      * @return
@@ -45,17 +55,16 @@ public class UserIdentityContract implements ContractInterface {
      * @param ctx
      * @param userSignId
      * @param userData
-     *
      */
 
     @Transaction()
     public void createUserIdentity(Context ctx, String userSignId, String userData) throws IOException {
-        boolean exists = userIdentityExists(ctx,userSignId);
+        boolean exists = userIdentityExists(ctx, userSignId);
         if (exists) {
-            throw new RuntimeException("The asset "+userSignId+" already exists");
+            throw new RuntimeException("The asset " + userSignId + " already exists");
         }
         UserIdentity userIdentity = UserIdentity.fromJSONString(userData);
-       // ctx.getStub().putState(userSignId, userIdentity.toJSONString().getBytes(UTF_8));
+        // ctx.getStub().putState(userSignId, userIdentity.toJSONString().getBytes(UTF_8));
 
         ctx.getStub().putStringState(userSignId, userIdentity.toJSONString());
     }
@@ -71,18 +80,18 @@ public class UserIdentityContract implements ContractInterface {
 
     @Transaction()
     public UserIdentity readUserIdentity(Context ctx, String userId) throws IOException {
-        boolean exists = userIdentityExists(ctx,userId);
+        boolean exists = userIdentityExists(ctx, userId);
         if (!exists) {
-            throw new RuntimeException("The asset "+ userId +" does not exist");
+            throw new RuntimeException("The asset " + userId + " does not exist");
         }
         //UserIdentity userIdentity = UserIdentity.fromJSONString(new String(ctx.getStub().getStringState(userId));
-        UserIdentity userIdentity = UserIdentity.fromJSONString(new String(ctx.getStub().getState(userId),UTF_8));
+        UserIdentity userIdentity = UserIdentity.fromJSONString(new String(ctx.getStub().getState(userId), UTF_8));
         return userIdentity;
     }
 
     @Transaction()
     public void updateUserIdentity(Context ctx, String userSignId, String newValue) throws IOException {
-        boolean exists = userIdentityExists(ctx,userSignId);
+        boolean exists = userIdentityExists(ctx, userSignId);
         if (!exists) {
             throw new RuntimeException("The asset " + userSignId + " does not exist");
         }
@@ -93,11 +102,33 @@ public class UserIdentityContract implements ContractInterface {
 
     @Transaction()
     public void deleteUserIdentity(Context ctx, String digitalSignId) {
-        boolean exists = userIdentityExists(ctx,digitalSignId);
+        boolean exists = userIdentityExists(ctx, digitalSignId);
         if (!exists) {
-            throw new RuntimeException("The asset "+digitalSignId+" does not exist");
+            throw new RuntimeException("The asset " + digitalSignId + " does not exist");
         }
         ctx.getStub().delState(digitalSignId);
+    }
+
+    @Transaction
+    public String query(Context context, String query) throws IOException {
+        List<UserIdentity> userList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Iterator<KeyValue> interator = context.getStub().getQueryResult(query).iterator();
+        while (interator.hasNext()) {
+
+            String key = interator.next().getKey();
+            UserIdentity identity = null;
+            identity = readUserIdentity(context, key);
+            userList.add(identity);
+        }
+        try {
+            return objectMapper.writeValueAsString(userList);
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(UserIdentity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "[]";
+
     }
 
 
